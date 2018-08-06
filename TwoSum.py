e7fc58aa416d1407ab131fee4791ae968ec3cce2
LSL <thulishulin@163.com>
2018-08-06 16:29:17 +0800

    Two Sum

---
+++ TwoSum.py
@@ -0,0 +1,14 @@
+class Solution:
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        for i in range(len(nums)):
+            diff = target-nums[i]
+            if diff in nums:
+                j = nums.index(diff)
+                if i!= j:
+                    return [i,j]
+            else: continue
