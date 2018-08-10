f3896e6e4511b7d1c22d7e9525be6c62781fc11f
LSL <thulishulin@163.com>
2018-08-10 12:11:46 +0800

    v2.0 -- other's version

--- LeetCodes/romanToInt.py
+++ LeetCodes/romanToInt.py
@@ -12,18 +12,18 @@ class Solution:
         :rtype: int
         """
         table = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
-        i = 0;result = 0
-        matches = {('I','V'),('I','X'),('X','L'),('X','C'),('C','D'),('C','M')}
-        while i < len(s) -1:
-            if (s[i],s[i+1]) in matches:
-                result += table[s[i+1]] - table[s[i]]
-                i += 2
+        result = 0
+        prefix = 0
+        current_val = 0
+        for i in range(len(s)-1,-1,-1):
+            current_val = table[s[i]]
+            if current_val < prefix:
+                result -= table[s[i]]
             else:
                 result += table[s[i]]
-                i += 1
-        if i == len(s) -1:
-            result += table[s[-1]]
+            prefix = current_val
         return result
+            
 if __name__ == '__main__':
     a = Solution()
-    print(a.romanToInt('MCMXCIV'))
+    print(a.romanToInt(''))
