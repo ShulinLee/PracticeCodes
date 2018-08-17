fd1b1b475f7f567c9c5c235703fddee12b2e6189
LSL <thulishulin@163.com>
2018-08-17 10:32:36 +0800

    other's version

--- LeetCodes/merge.py
+++ LeetCodes/merge.py
@@ -14,33 +14,26 @@ class Solution:
         :type n: int
         :rtype: void Do not return anything, modify nums1 in-place instead.
         """
-        if n == 0:return
-        i = 0
-        if nums2[0] <= nums1[0]:
-                nums1.insert(0,nums2[0])
+        i = 0;j = 0;inserted = 0
         while i < n:
-            if nums2[i] >= nums1[m+i-1]:
-                for s in nums2[i:]:
-                    nums1.insert(m+i,s)
+            if j < m + inserted:
+                if nums1[j] > nums2[i]:
+                    nums1.insert(j,nums2[i])
                     i += 1
-                break
-            left = 0;right = m+i-1;flag = True
-            while left < right:
-                mid = (left + right) // 2
-                if nums2[i] == nums1[mid]:
-                    nums1.insert(mid,nums2[i])
-                    flag = False
-                    break
-                elif nums2[i] > nums1[mid]:
-                    left = mid + 1
-                else: right = mid - 1
-            if flag:
-                nums1.insert(left,nums2[i]) if nums1[left] > nums2[i] else nums1.insert(left+1,nums2[i])
-            i += 1
-        return 
-if __name__ == '__main__':
+                    j += 1
+                    inserted += 1
+                    nums1.pop()
+                else: j += 1
+            else:
+                nums1.insert(j,nums2[i])
+                i += 1
+                j += 1
+                inserted += 1
+                nums1.pop()
+#test                
+if __name__ == '__main__':    
     a = Solution()
-    nums1 = [1,2,3,0,0,0]
-    nums2 = [2,5,6]
-    m = 3;n = 3
+    nums1 = []
+    nums2 = []
+    m = 0;n = 0
     a.merge(nums1,m,nums2,n)
