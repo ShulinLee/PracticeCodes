cf47c1e6f301487c93f14541f4a66cc1a56ccd26
LSL <thulishulin@163.com>
2018-09-14 15:31:47 +0800

    Nim Game

---
+++ LeetCodes/canWinNim.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 14 15:22:37 2018
+
+@author: shuli
+"""
+
+class Solution:
+    def canWinNim(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        return n%4 != 0
